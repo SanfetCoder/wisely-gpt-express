68d70540dca66242cc25bb43fa1bac8b68976fc9
SanphetLovesTina <sanphetlovestina@SanphetLovesTinas-MacBook-Air.local>
2023-08-15 16:10:33 +0700

    Init

--- server.js
+++ server.js
@@ -16,12 +16,13 @@ app.use(cors({
   origin: 'http://localhost:5173' // Change this to match your React app's origin
 }));
 
-app.route("/")
+app.route("/:question")
     .post(async(req,res)=>{
+      const question = req.params.question;
       // GPT
       const chatCompletion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: "Hello world"}],
+        messages: [{role: "user", content: question}],
       });
       const response = chatCompletion.data.choices[0].message.content
       res.send(response)
